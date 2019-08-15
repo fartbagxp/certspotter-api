c6653899482d7eb40299f4901187730f4f88af4a
fartbagxp <fartbagxp@gmail.com>
2019-08-15 03:52:46 -0400

    Fix deployment, bump version (#5)
    * Fixing deployment script
    
    * Adding v1.0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 from setuptools import setup
 from setuptools.command.install import install
 
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 
 
 def readme():
@@ -18,7 +18,7 @@ def readme():
 
 setup(name='certspotter',
       version=VERSION,
-      description='sslmate CertSpotter API'
+      description='sslmate CertSpotter API',
       long_description=readme(),
       url="https://github.com/fartbagxp/certspotter-api"
       author='Boris Ning',
