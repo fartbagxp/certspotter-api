1fe5aae649e20dffdfbe97064c46915eed0cb422
Boris Ning <fartbagxp@gmail.com>
2020-12-30 17:18:28 -0500

    Fix testing and updating dependencies/circleci configurations

--- src/test/test_api.py
+++ src/test/test_api.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 import os
 
 from certspotter.api import CertSpotter
@@ -6,11 +7,29 @@ from certspotter.api import CertSpotter
 
 class TestBasicFunction(unittest.TestCase):
 
-  def certspot(self, site):
+  def throttle_getdomains(self, site, after=0):
     api = CertSpotter(os.environ["CERTSPOTTER_TOKEN"])
+    if after == 0:
+      subdomains, retryinsec = api.getdomains([site])
+      if retryinsec != 0:
+        print(f'Being throttled, waiting {retryinsec} seconds')
+        time.sleep(int(retryinsec)+1)
+        subdomains, retryinsec = api.getdomains([site])
+        return subdomains, retryinsec
+      return subdomains, retryinsec
+    else:
+      subdomains, retryinsec = api.getdomains([site], after)
+      if retryinsec != 0:
+        print(f'Being throttled, waiting {retryinsec} seconds')
+        time.sleep(int(retryinsec)+1)
+        subdomains, retryinsec = api.getdomains([site], after)
+        return subdomains, retryinsec
+      return subdomains, retryinsec
+
+  def certspot(self, site):
     subdomains_list = []
 
-    subdomains, retryinsec = api.getdomains([site])
+    subdomains, retryinsec = self.throttle_getdomains(site)
     numEntries = len(subdomains)
     totalEntries = 0
     totalEntries += numEntries
@@ -21,7 +40,7 @@ class TestBasicFunction(unittest.TestCase):
       print(f"Retry after: {retryinsec} seconds.")
 
     while numEntries > 0:
-      subdomains, retryinsec = api.getdomains([site], after)
+      subdomains, retryinsec = self.throttle_getdomains(site, after)
       numEntries = len(subdomains)
       subdomains_list.extend(subdomains)
 
@@ -36,14 +55,12 @@ class TestBasicFunction(unittest.TestCase):
     return totalEntries, retryinsec
 
   def test_good_case(self):
-    totalEntries, retryinsec = self.certspot('dhs.gov')
+    totalEntries, retryinsec = self.certspot('example.com')
     self.assertFalse(totalEntries == 0)
-    self.assertFalse(retryinsec == 0)
 
   def test_bad_case(self):
     totalEntries, retryinsec = self.certspot('dfkslfjaekx.com')
     self.assertTrue(totalEntries == 0)
-    self.assertFalse(retryinsec == 0)
 
 
 if __name__ == '__main__':
